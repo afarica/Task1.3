d4b00705b3fbb4ef8f471302b92f153bb75fd50f
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 02:09:10 +0600

    Update p13.py

--- p13.py
+++ p13.py
@@ -1,7 +1,8 @@
+# Write a function is_odd that takes
+# in a number and returns True if it is odd, otherwise false.
 a=int(input("Enter your number:")) 
 c=a%2
 if c==0: 
 	print("This number is even")
 elif c!=0: 
-	print("This number is odd")
-	
+	print("This number is odd")	
